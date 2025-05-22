c939531f0753cc68e02efa98ef0187d37d646f79
bishalrajbhattarai012 <bishal.bhattara.ebpearls@gmail.com>
2025-05-22 10:34:16 +0545

    async code and mocks

--- testing/util.js
+++ testing/util.js
@@ -1,5 +1,22 @@
 module.exports.add = (a, b) => a + b;
 
 module.exports.throwAnError = () => {
-    throw new Error("Dummy Error")
+  throw new Error("Dummy Error");
 };
+
+function fetchData(callback) {
+  setTimeout(() => {
+    callback("Data is Fetched after 1000 ms");
+  }, 1000);
+}
+
+function fetchDataPromise() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Data is Fetch with promise after 10000 ms");
+    });
+  });
+}
+
+module.exports.fetchData = fetchData;
+exports.fetchDataPromise = fetchDataPromise
--- testing/util.test.js
+++ testing/util.test.js
@@ -1,14 +1,39 @@
-const {add,throwAnError} = require("./util")
+const { add, throwAnError, fetchData, fetchDataPromise } = require("./util");
 // import {add} from "./util"
 
-test("should return 2",()=>{
-    expect(add(1,1)).toBe(2)
-})
+// basic matchers
+test("should return 2", () => {
+  expect(add(1, 1)).toBe(2);
+});
 
+test("should throw an error", () => {
+  expect(() => {
+    throwAnError();
+  }).toThrow();
+});
 
-test("should throw an error",()=>{
-    expect(()=>{
-        throwAnError()
-    }).toThrow();
+// testing async callback code simulating with setTimeout()
+test("async code should return fetched data message", (done) => {
+  function callback(data) {
+    try {
+      expect(data).toBe("Data is Fetched after 1000 ms");
+      done();
+    } catch (error) {
+      done(error);
+    }
+  }
+  fetchData(callback);
+});
 
-})
+// Unit Testing foe promise based function
+test("async code promise should return fetch data promise message", () => {
+  expect(fetchDataPromise()).resolves.toBe(
+    "Data is Fetch with promise after 10000 ms"
+  );
+
+});
+
+test("async code promise should return fetch data promise message", async () => {
+  const data = await fetchDataPromise();
+  expect(data).toBe("Data is Fetch with promise after 10000 ms");
+});
