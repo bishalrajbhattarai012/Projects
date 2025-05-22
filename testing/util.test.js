6d7aea565a6784680625817373a4e26e20380800
bishalrajbhattarai012 <bishal.bhattara.ebpearls@gmail.com>
2025-05-22 09:19:18 +0545

    basic matches

--- testing/util.test.js
+++ testing/util.test.js
@@ -1,6 +1,14 @@
-const {add} = require("./util")
+const {add,throwAnError} = require("./util")
+// import {add} from "./util"
 
 test("should return 2",()=>{
     expect(add(1,1)).toBe(2)
 })
 
+
+test("should throw an error",()=>{
+    expect(()=>{
+        throwAnError()
+    }).toThrow();
+
+})
